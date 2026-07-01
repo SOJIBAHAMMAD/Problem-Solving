80557ccdf69bd1df3ce294f9c7f9b80ec6c5bf7c
MD. SOJIB <118648607+SOJIBAHAMMAD@users.noreply.github.com>
2026-07-02 00:04:39 +0600

    Add files via upload

---
+++ Week 1 ( Basics of CP)/Problem Set/L-Quadrant_Selection.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+    int X,Y;
+    cin>>X>>Y;
+    if(X>0 && Y>0) {
+        cout<<1<<endl;
+    } else if(X<0 && Y>0) {
+        cout<<2<<endl;
+    } else if(X<0 && Y<0) {
+        cout<<3<<endl;
+    } else {
+        cout<<4<<endl;
+    }
+    return 0;
+}
