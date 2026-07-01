f75ac6f87cda2fe91a6d41734793e834461608fc
MD. SOJIB <118648607+SOJIBAHAMMAD@users.noreply.github.com>
2026-07-02 00:05:03 +0600

    Add files via upload

---
+++ Week 1 ( Basics of CP)/Problem Set/M-Solving_for_Carrots.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+    int N,P;
+    cin>>N>>P;
+    cin.ignore();
+    string s1,s2;
+    cin>>s1>>s2;
+    cout<<P<<endl;
+    return 0;
+}
