8a8c251b95e6994ff46e4a62645d9cd1ad7c5f09
MD. SOJIB <118648607+SOJIBAHAMMAD@users.noreply.github.com>
2026-07-05 00:09:05 +0600

    Add files via upload

---
+++ Week 1 ( Basics of CP)/Problem Set/P-Quality-Adjusted_Life-Year.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+    int N;
+    cin>>N;
+    double ans=0.0;
+    for(int i=0;i<N;i++) {
+        double q,y;
+        cin>>q>>y;
+        ans+=q*y;
+    }
+    cout<<fixed<<setprecision(3)<<ans<<endl;
+    return 0;
+}
