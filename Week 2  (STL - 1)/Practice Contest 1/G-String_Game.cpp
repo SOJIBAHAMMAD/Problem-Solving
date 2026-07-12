ca42320ddab70a13ed8902e5f174b51aa74bdb5a
MD. SOJIB <118648607+SOJIBAHAMMAD@users.noreply.github.com>
2026-07-13 00:10:42 +0600

    Add files via upload

---
+++ Week 2  (STL - 1)/Practice Contest 1/G-String_Game.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t;
+    cin>>t;
+    while(t--) {
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        int count=0;
+        for(int i=0;i<n-1;i++) {
+            if(s[i]!=s[i+1]) {
+                count++;
+            } 
+        }
+        if(count % 2==0) {
+            
+        }
+    }
+    return 0;
+}
