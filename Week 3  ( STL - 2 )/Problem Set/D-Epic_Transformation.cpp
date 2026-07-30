842b50fb7af2e651d67af56c47b0f40df6ee0ea0
MD. SOJIB <118648607+SOJIBAHAMMAD@users.noreply.github.com>
2026-07-31 00:08:02 +0600

    Add files via upload

---
+++ Week 3  ( STL - 2 )/Problem Set/D-Epic_Transformation.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t;
+    cin>>t;
+    while(t--) {
+        int n;
+        cin>>n;
+        map<int ,int>mp;
+        for(int i=1;i<=n;i++) {
+            int x;
+            cin>>x;
+            mp[x]++;
+        }
+        priority_queue<int>pq;
+        for(auto val : mp) {
+            pq.push(val.second);  // we need to insert freq of ele. into pq;
+        }
+
+        while(!pq.empty()) {
+            if(pq.size()<2) {
+                break;
+            }
+            int x,y;
+            x=pq.top();
+            pq.pop();
+            y=pq.top();
+            pq.pop();
+            x--,y--;
+            if(x > 0) {
+                pq.push(x);
+            }
+            if(y > 0) {
+                pq.push(y);
+            }
+        }
+        int ans=0;
+        while(!pq.empty()) {
+            ans+=pq.top();
+            pq.pop();
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
