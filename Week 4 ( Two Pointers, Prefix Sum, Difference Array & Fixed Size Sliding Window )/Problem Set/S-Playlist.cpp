94fc7423977eded84baacd76a59724268f088307
MD. SOJIB <118648607+SOJIBAHAMMAD@users.noreply.github.com>
2026-08-14 00:18:43 +0600

    Add files via upload

---
+++ Week 4 ( Two Pointers, Prefix Sum, Difference Array & Fixed Size Sliding Window )/Problem Set/S-Playlist.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n;
+    cin>>n;
+    map<int,int>last_idx;
+    int i=1;
+    int ans=0;
+    for(int j=1;j<=n;j++) {
+        int x;
+        cin>>x;
+        i=max(i,last_idx[x]+1);
+        last_idx[x]=j;
+        ans=max(ans,j-i+1);
+    }
+    cout<<ans<<endl;
+    return 0;
+}
