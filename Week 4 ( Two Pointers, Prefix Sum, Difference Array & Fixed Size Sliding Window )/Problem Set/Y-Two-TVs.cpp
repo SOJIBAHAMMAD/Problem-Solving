de3e48edd387b2a8d4f9317c6939875e6c107c4a
MD. SOJIB <118648607+SOJIBAHAMMAD@users.noreply.github.com>
2026-08-16 00:07:55 +0600

    Add files via upload

---
+++ Week 4 ( Two Pointers, Prefix Sum, Difference Array & Fixed Size Sliding Window )/Problem Set/Y-Two-TVs.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+
+    int n;
+    cin>>n;
+    map<int,int>d;
+    for(int i=1;i<=n;i++) {
+        int l,r;
+        cin>>l>>r;
+        d[l]++;
+        d[r+1]--;
+    }
+
+    long long sum=0;
+    bool flag=true;
+    for(auto val : d) {
+        sum+= val.second;
+        if(sum>2) {
+            flag=false;
+            break;
+        }
+    }
+    if(flag) {
+        cout<<"YES"<<endl;
+    } else {
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
