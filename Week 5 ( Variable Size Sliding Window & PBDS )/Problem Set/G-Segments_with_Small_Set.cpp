72aafccda8687fef8264b8c7c3f577a4f21b72ff
MD. SOJIB <118648607+SOJIBAHAMMAD@users.noreply.github.com>
2026-08-21 00:12:06 +0600

    Add files via upload

---
+++ Week 5 ( Variable Size Sliding Window & PBDS )/Problem Set/G-Segments_with_Small_Set.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main () {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    ll n,k;
+    cin>>n>>k;
+    vector<ll>v(n);
+    for(ll i=0;i<n;i++) {
+        cin>>v[i];
+    }
+    ll l=0,r=0,ans=0;
+    map<ll,ll>mp;
+    while(r<n) {
+        mp[v[r]]++;
+        if(mp.size() <= k) {
+            ans += (r-l+1);
+        } else {
+            while(mp.size() > k && l<=r) {
+                mp[v[l]]--;
+                if(mp[v[l]] == 0) {
+                    mp.erase(v[l]);
+                }
+                l++;
+            }
+            if(mp.size() <= k) {
+                ans += (r-l+1);
+            }
+        }
+        r++;
+    }
+    cout<<ans<<endl;
+    return 0;
+}
