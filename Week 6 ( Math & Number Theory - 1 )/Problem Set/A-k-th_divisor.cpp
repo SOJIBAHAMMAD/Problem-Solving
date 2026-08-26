d4b21bf422252c3ce2783e6961b9cb4f67f0eb63
MD. SOJIB <118648607+SOJIBAHAMMAD@users.noreply.github.com>
2026-08-27 00:33:44 +0600

    Add A-k-th_divisor.cpp for finding k-th divisor

---
+++ Week 6 ( Math & Number Theory - 1 )/Problem Set/A-k-th_divisor.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main () {
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+
+   ll n;
+   int k;
+   cin>>n>>k;
+   vector<ll>a;
+   for(ll i=1;i*i<=n;i++) {
+    if(n%i == 0) {
+        a.push_back(i);
+        if(n/i != i) {
+            a.push_back(n/i);
+        }
+    }
+   }
+   sort(a.begin(),a.end());
+    if(a.size() < k) {
+        cout<<-1<<endl;
+    } else {
+        cout<<a[k-1]<<endl;
+    }
+   return 0;
+}
