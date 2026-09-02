d95182abd36d03dcb16501afd28a35ba481f2b77
MD. SOJIB <118648607+SOJIBAHAMMAD@users.noreply.github.com>
2026-09-03 00:06:14 +0600

    Add files via upload

---
+++ Week 7 ( Bit Manipulation - 1 )/Module 7 (Bit Manipulation - 1 )/Two_Power_calculation_using_LS.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main () {
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+
+   int k;
+   cin>>k;
+   cout<< (1 << k)<<endl; // using left shift
+   return 0;
+}
