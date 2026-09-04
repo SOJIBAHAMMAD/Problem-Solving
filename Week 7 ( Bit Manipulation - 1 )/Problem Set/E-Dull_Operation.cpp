329ab03f04afc8b399490cccd93c3789e48bed01
MD. SOJIB <118648607+SOJIBAHAMMAD@users.noreply.github.com>
2026-09-04 23:39:12 +0600

    Add files via upload

---
+++ Week 7 ( Bit Manipulation - 1 )/Problem Set/E-Dull_Operation.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl '\n'
+int main () {
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+
+   int t;
+   cin>>t;
+   while(t--) {
+    int n;
+    cin>>n;
+    cout<<n<<" "<<n-1<<nl;
+   }
+   return 0;
+}
