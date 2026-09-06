fbb87c7504860e33e43a4d6da399c76173d0da82
MD. SOJIB <118648607+SOJIBAHAMMAD@users.noreply.github.com>
2026-09-07 00:09:54 +0600

    Add files via upload

---
+++ Week 7 ( Bit Manipulation - 1 )/Problem Set/Q-Mocha_and_Math.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl '\n'
+int main () {
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+
+   int t;
+   cin>>t;
+   while(t--) {
+    int n;
+    cin>>n;
+    vector<int>a(n);
+    for(int i=0;i<n;i++) {
+        cin>>a[i];
+    }
+    int ans=a[0];
+    for(int i=1;i<n;i++) {
+        ans &= a[i];
+    }
+    cout<<ans<<nl;
+   }
+   return 0;
+}
