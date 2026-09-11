454b2953281adbdd2de8f2b5639c3eda90bcc2c1
MD. SOJIB <118648607+SOJIBAHAMMAD@users.noreply.github.com>
2026-09-12 00:17:26 +0600

    Add files via upload

---
+++ Week 8 ( Searching & Sorting - 1 )/Problem Set/Last_Occurrence.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl '\n'
+int main () {
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+
+   int n,key;
+   cin>>n>>key;
+   vector<int>a(n);
+   for(int i=0;i<n;i++) {
+    cin>>a[i];
+   }
+   int l=0,r=n-1;
+   int ans=-1;
+   while(l<=r) {
+    int mid = l + (r-l)/2;
+    if(key == a[mid]) {
+        ans = mid;
+        l = mid +1;
+    } else if(key < a[mid]) {
+        r = mid -1;
+    } else {
+        l = mid +1;
+    }
+   }
+   cout<<ans+1<<nl; // 1 based indexing 
+   return 0;
+}
