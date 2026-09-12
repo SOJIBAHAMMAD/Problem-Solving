7391fefe38f9dae8b6165861a025a5903ed05531
MD. SOJIB <118648607+SOJIBAHAMMAD@users.noreply.github.com>
2026-09-13 00:24:12 +0600

    Add files via upload

---
+++ Week 8 ( Searching & Sorting - 1 )/Problem Set/Power_of_two.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl '\n'
+int main () {
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+
+   int n;
+   cin>>n;
+   cout<<(1 << n)<<nl;
+   return 0;
+}
